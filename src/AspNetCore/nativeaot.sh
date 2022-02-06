a5aff6c81aedfa8b0ac0baf099beabe63f045a92
Andrii Kurdiumov <kant2002@gmail.com>
2022-02-06 23:18:01 +0600

    Apply PR feedback

--- src/AspNetCore/nativeaot.sh
+++ src/AspNetCore/nativeaot.sh
@@ -3,6 +3,6 @@
 cd Example
 dotnet restore
 dotnet publish -c Release -r linux-x64 -p:PublishNativeAot=True
-# Uncomment to get rid of debugging symbols in executable
-#strip bin/Release/net6.0/linux-x64/publish/Example
+# Comment line below if you want preserve debug symbols in the executable
+strip bin/Release/net6.0/linux-x64/publish/Example
 bin/Release/net6.0/linux-x64/publish/Example
