8b279d8684b870dbc90d49955411aec066709d85
revazashvili <levanrevazashvili@gmail.com>
2022-06-19 11:53:16 +0400

    add missed namespace

--- src/AspNetCoreController/Example/Startup.cs
+++ src/AspNetCoreController/Example/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using StarWars;
 
 namespace Example
